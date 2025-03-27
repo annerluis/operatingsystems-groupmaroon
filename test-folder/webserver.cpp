fd84acf200a93794e5f34021029af3d5889f7afd
Olivia <obrunt@uwo.ca>
2025-03-27 14:20:35 -0400

    Changed example file for httplib.h server

--- test-folder/webserver.cpp
+++ test-folder/webserver.cpp
@@ -6,7 +6,7 @@
 
 #include <httplib.h>
 
-/* https://stackoverflow.com/questions/70141812/how-to-run-cprogram-at-the-backend-of-a-web-application */
+/* https://github.com/yhirose/cpp-httplib/ */
 
 
 int main(void)
@@ -15,14 +15,54 @@ int main(void)
 
     Server svr;
 
-    svr.Get(R"(/numbers/(\d+))", [&](const Request &req, Response &res)
-            {
-            auto numbers = req.matches[1];
-            res.set_content(numbers, "text/plain");
-        });
+    svr.Get("/hi", [](const Request& req, Response& res) {
+        res.set_content("Hello World!", "text/plain");
+    });
 
-    svr.Get("/stop", [&](const Request &req, Response &res)
-            { svr.stop(); });
+    // Match the request path against a regular expression
+    // and extract its captures
+    svr.Get(R"(/numbers/(\d+))", [&](const Request& req, Response& res) {
+        auto numbers = req.matches[1];
+        res.set_content(numbers, "text/plain");
+    });
 
-    svr.listen("localhost", 6000);
+    // Capture the second segment of the request path as "id" path param
+    svr.Get("/users/:id", [&](const Request& req, Response& res) {
+        auto user_id = req.path_params.at("id");
+        res.set_content(user_id, "text/plain");
+    });
+
+    // Extract values from HTTP headers and URL query params
+    svr.Get("/body-header-param", [](const Request& req, Response& res) {
+        if (req.has_header("Content-Length")) {
+            auto val = req.get_header_value("Content-Length");
+        }
+        if (req.has_param("key")) {
+            auto val = req.get_param_value("key");
+        }
+        res.set_content(req.body, "text/plain");
+    });
+
+    // If the handler takes time to finish, you can also poll the connection state
+    svr.Get("/task", [&](const Request& req, Response& res) {
+        const char * result = nullptr;
+        process.run(); // for example, starting an external process
+        while (result == nullptr) {
+            sleep(1);
+            if (req.is_connection_closed()) {
+                process.kill(); // kill the process
+                return;
+            }
+            result = process.stdout(); // != nullptr if the process finishes
+        }
+        res.set_content(result, "text/plain");
+    });
+
+    svr.Get("/stop", [&](const Request& req, Response& res) {
+        svr.stop();
+    });
+
+    int port = svr.bind_to_any_port("0.0.0.0");
+    svr.listen_after_bind();
+    svr.listen("localhost", port);
 }
